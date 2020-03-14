1bad6c3e4c8daa9df4d815d45346ab066c4c32e7
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-03-14 19:23:40 +0700

    Implememnted StoryLine.java

---
+++ src/elements/StoryLine.java
@@ -0,0 +1,18 @@
+package elements;
+
+import java.awt.*;
+import java.time.LocalDateTime;
+
+public class StoryLine extends BasicStoryElement {
+    LocalDateTime period;
+
+    public StoryLine(String title, String description, Color color, TimePeriod timePeriod, LocalDateTime period) {
+        super(title, description, color, timePeriod);
+        this.period = period;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
