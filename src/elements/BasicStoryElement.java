2a1cff48ec93cb3cc5cbf54193ba8d73223756f5
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-03-14 18:29:09 +0700

    Change LocalDateTime to TimePeriod Object

--- src/elements/BasicStoryElement.java
+++ src/elements/BasicStoryElement.java
@@ -1,6 +1,52 @@
 package elements;
 
-public class BasicStoryElement {
-    public String title;
-    public String description;
+import java.awt.*;
+
+public abstract class BasicStoryElement {
+    protected String title;
+    protected String description;
+    protected Color color;
+    protected TimePeriod timePeriod;
+
+    public BasicStoryElement(String title, String description, Color color, TimePeriod timePeriod) {
+        this.title = title;
+        this.description = description;
+        this.color = color;
+        this.timePeriod = timePeriod;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public TimePeriod getTimePeriod() {
+        return timePeriod;
+    }
+
+    public void setTimePeriod(TimePeriod timePeriod) {
+        this.timePeriod = timePeriod;
+    }
+
+    @Override
+    abstract public String toString();
 }
