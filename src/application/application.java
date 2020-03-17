888373367540a924e8ed76df46cfe7db84bcdbd0
Nattamon Polvichai <joppy.pgh@gmail.com>
2020-03-17 19:13:00 +0700

    Add support for JavaFX

--- src/application/application.java
+++ src/application/application.java
@@ -1,4 +1,23 @@
 package application;
 
-public class application {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class application extends Application {
+
+    @Override
+    public void start(Stage mainStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mainStage.fxml"));
+        mainStage.setTitle("Chronology");
+        mainStage.setMaximized(true);
+        mainStage.setScene(new Scene(root, 300, 275));
+        mainStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
