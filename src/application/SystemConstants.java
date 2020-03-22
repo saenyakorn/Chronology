8f99b598de48c2ebcc60af92c22d63edf2d0c04d
Nattamon Polvichai <joppy.pgh@gmail.com>
2020-03-22 20:12:29 +0700

    Add sidebar width as constant

--- src/application/SystemConstants.java
+++ src/application/SystemConstants.java
@@ -2,8 +2,11 @@ package application;
 
 public class SystemConstants {
 
-    /* This provide application constant values */
+    /* Application constants */
     public static final String APP_NAME = "Chronology";
+    public static final int SIDEBAR_PREF_WIDTH = 200;
+
+    /*Color scheme constants*/
     public static final String PRIMARY_COLOR = "#FFFFFF";
     public static final String SECONDARY_COLOR = "#000000";
 }
