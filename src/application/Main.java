6a427576e6c4108fb5a70606c972403cabe0ee09
Nattamon Polvichai <joppy.pgh@gmail.com>
2020-03-22 17:18:25 +0700

    Rename Main class

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class application extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage mainStage) throws Exception {
