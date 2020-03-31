d1862caaf1332a6962e4a26ee506dc92a6bc183f
Nattamon Polvichai <joppy.pgh@gmail.com>
2020-04-01 00:34:40 +0700

    Add color scheme constants

--- src/application/SystemConstants.java
+++ src/application/SystemConstants.java
@@ -7,6 +7,19 @@ public class SystemConstants {
     public static final int SIDEBAR_PREF_WIDTH = 200;
 
     /*Color scheme constants*/
-    public static final String PRIMARY_COLOR = "#FFFFFF";
-    public static final String SECONDARY_COLOR = "#000000";
+    //Monochrome
+    public static final String WHITE = "#FFFFFF";
+    public static final String LIGHT_GRAY = "#F6F6F6";
+    public static final String MEDIUM_GRAY = "#4F4F4F";
+    public static final String DARK_GRAY = "#6B6B6B";
+    public static final String BLACK = "#1C1C1C";
+
+    //Default palette
+    public static final String PINK = "#FF76B8";
+    public static final String RED = "#FF3E60";
+    public static final String ORANGE = "#FFA63E";
+    public static final String LIME = "#94EE6A";
+    public static final String SKY_BLUE = "#3EC5FF";
+    public static final String LAVENDER = "#A587FB";
+
 }
