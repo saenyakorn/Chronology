c40a4365c1e817cc76634b6c0851a5565d4d650f
Nattamon Polvichai <joppy.pgh@gmail.com>
2020-04-01 21:58:26 +0700

    Add style class for EventCard

---
+++ src/component/components/eventCard/EventCardStyle.java
@@ -0,0 +1,8 @@
+package component.components.eventCard;
+
+public class EventCardStyle {
+
+    public static final String PADDING = "-fx-padding: 10 15 10 15;";
+    public static String BEGINDATE_TEXT = "-fx-font: 24px Noto Sans Bold;";
+    public static final String BEGINTIME_TEXT = "-fx-font: 14px Noto Sans Bold";
+}
