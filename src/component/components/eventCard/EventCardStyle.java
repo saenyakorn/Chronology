85f390e29fc0ea38ca2b5dc648f4a631af2d5b81
Nattamon Polvichai <joppy.pgh@gmail.com>
2020-04-02 00:37:43 +0700

    Partially add styles to EventCard

--- src/component/components/eventCard/EventCardStyle.java
+++ src/component/components/eventCard/EventCardStyle.java
@@ -1,8 +1,19 @@
 package component.components.eventCard;
 
-public class EventCardStyle {
+import application.SystemConstants;
+
+package class EventCardStyle {
 
     public static final String PADDING = "-fx-padding: 10 15 10 15;";
+    public static final String DROP_SHADOW = SystemConstants.DROP_SHADOW;
+    
+    public static final String WHITE_BACKGROUND = "-fx-background-color: " + SystemConstants.WHITE + ";";
+    public static String COLOR_BACKGROUND = "-fx-background-color: " + "" + ";";
+    public static final String WHITE_TEXT = "-fx-fill: " + SystemConstants.WHITE + ";";
+    public static final String BLACK_TEXT = "-fx-fill: " + SystemConstants.BLACK + ";";
+    
+    //TODO : Add font weights to SystemConstants
     public static String BEGINDATE_TEXT = "-fx-font: 24px Noto Sans Bold;";
-    public static final String BEGINTIME_TEXT = "-fx-font: 14px Noto Sans Bold";
+    public static String BEGINTIME_TEXT = "-fx-font: 14px Noto Sans Bold";
+
 }
