7287b98c62a5a552e31cfefa606a222860d2a769
Nattamon Polvichai <joppy.pgh@gmail.com>
2020-04-04 22:28:34 +0700

    Add font constants to SystemConstants

--- src/application/SystemConstants.java
+++ src/application/SystemConstants.java
@@ -9,7 +9,23 @@ public class SystemConstants {
     /* Styling constants */
     public static final int SPACING = 8;
     public static final int BORDER_RADIUS = 20;
-    public static final String DROP_SHADOW = " -fx-effect: dropshadow(gaussian, rgba(0,0,0,0.2), 10, 0, 0, 10);";
+    public static final String DROP_SHADOW = " -fx-effect: dropshadow(gaussian, rgba(28,28,28,0.2), 10, 0, 0, 10);";
+
+    /* Font Constants */
+    public static final String FONT_REGULAR_PATH = "file:res/fonts/NotoSansThai-Regular.ttf";
+    public static final String FONT_BOLD_PATH = "file:res/fonts/NotoSansThai-Bold.ttf";
+    public static final int HEADING_WEIGHT = 14;
+    public static final int REGULAR_WEIGHT = 12;
+
+    public static final String LOADFONT_REGULAR = "@font-face {" +
+            "    font-family: NotoSansRegular;" +
+            "    src: url('" + FONT_REGULAR_PATH + "');" +
+            "}";
+
+    public static final String LOADFONT_BOLD = "@font-face {" +
+            "    font-family: NotoSansBold;" +
+            "    src: url('" + FONT_BOLD_PATH + "');" +
+            "}";
 
     /*Color scheme constants*/
     //Monochrome
