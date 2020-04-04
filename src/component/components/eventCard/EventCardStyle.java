ab1ffd01d6e72e2584639ae8e3e34e0e88227df7
Nattamon Polvichai <joppy.pgh@gmail.com>
2020-04-04 22:31:48 +0700

    Style with EventCardStyle class

--- src/component/components/eventCard/EventCardStyle.java
+++ src/component/components/eventCard/EventCardStyle.java
@@ -2,18 +2,21 @@ package component.components.eventCard;
 
 import application.SystemConstants;
 
-class EventCardStyle {
+public class EventCardStyle {
 
     public static final String PADDING = "-fx-padding: 10 15 10 15;";
     public static final String DROP_SHADOW = SystemConstants.DROP_SHADOW;
     
     public static final String WHITE_BACKGROUND = "-fx-background-color: " + SystemConstants.WHITE + ";";
-    public static String COLOR_BACKGROUND = "-fx-background-color: " + "" + ";";
+    public static final String DEFAULTCOLOR_BACKGROUND = "-fx-background-color: " + SystemConstants.RED + ";";
+
     public static final String WHITE_TEXT = "-fx-fill: " + SystemConstants.WHITE + ";";
     public static final String BLACK_TEXT = "-fx-fill: " + SystemConstants.BLACK + ";";
-    
-    //TODO : Add font weights to SystemConstants
-    public static String BEGINDATE_TEXT = "-fx-font: 24px Noto Sans Bold;";
-    public static String BEGINTIME_TEXT = "-fx-font: 14px Noto Sans Bold";
+    public static final String DEFAULTCOLOR_TEXT = "-fx-fill: " + SystemConstants.RED + ";";
+
+    public static final String BEGINDATE_TEXT = "-fx-font: 24 'NotoSansBold';";
+    public static final String BEGINTIME_TEXT = "-fx-font: 14 'NotoSansBold';";
+
+    public static final String ROUND_EDGES = "-fx-background-radius: " + SystemConstants.BORDER_RADIUS + ";";
 
 }
