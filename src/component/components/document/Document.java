6824a691bb508e589ff0d29b59558033d9eee822
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-04-08 00:54:25 +0700

    Included name props for document

--- src/component/components/document/Document.java
+++ src/component/components/document/Document.java
@@ -5,11 +5,13 @@ import component.components.eventCard.EventCardList;
 import component.components.storyline.StorylineList;
 
 public class Document {
+    private String name;
     private EventCardList eventCards;
     private ChapterList chapters;
     private StorylineList storylines;
 
-    public Document() {
+    public Document(String name) {
+        this.name = name;
         eventCards = new EventCardList();
         chapters = new ChapterList();
         storylines = new StorylineList();
