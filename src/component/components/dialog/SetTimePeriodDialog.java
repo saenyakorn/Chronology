858055dadd9f4f00daa6755c6088899a35f42f43
Nattamon Polvichai <joppy.pgh@gmail.com>
2020-04-08 19:58:43 +0700

    Add dialog for setting TimePeriod of EventCard

---
+++ src/component/components/dialog/SetTimePeriodDialog.java
@@ -0,0 +1,44 @@
+package component.components.dialog;
+
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class SetTimePeriodDialog extends Dialog {
+
+    public SetTimePeriodDialog() {
+        VBox root = new VBox();
+
+        HBox hbox = new HBox();
+        hbox.setSpacing(5);
+
+        ToggleGroup modeSelector = new ToggleGroup();
+        RadioButton predefined = new RadioButton("Use Predefined Times");
+        RadioButton custom = new RadioButton("Customize Times");
+        predefined.setToggleGroup(modeSelector);
+        custom.setToggleGroup(modeSelector);
+        predefined.setSelected(true);
+
+        hbox.getChildren().addAll(predefined,custom);
+
+        stage.setScene(new Scene(root,300,400));
+    }
+
+    private void predefinedMode() {
+
+    }
+
+    private void customMode() {
+        Text setBeginDatePrompt = new Text("Begin Date:");
+        Text setBeginTimePrompt = new Text("Begin Time:");
+        Text setEndDatePrompt = new Text("End Date:");
+        Text setEndTimePrompt = new Text("End Time:");
+
+        DatePicker beginDatePicker = new DatePicker();
+        DatePicker endDatePicker = new DatePicker();
+    }
+}
