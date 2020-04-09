cfc631b8bc0d998b1211d6d6394bc2cd115c386d
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-04-09 21:51:06 +0700

    Added viewer as component

---
+++ src/component/components/viewer/Viewer.java
@@ -0,0 +1,20 @@
+package component.components.viewer;
+
+import component.components.document.Document;
+import javafx.scene.control.TabPane;
+
+public class Viewer extends TabPane {
+
+    public Viewer() {
+
+    }
+
+    public void addDocument(Document document) {
+        System.out.println("Viewer: " + document.getText());
+        this.getTabs().add(document);
+    }
+
+    public void removeDocument(Document document) {
+        this.getTabs().remove(document);
+    }
+}
