1b95c8839a898668bbaa625907e89d157824fff4
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-04-09 22:10:31 +0700

    Refactor imported components

--- src/application/layout/mainController.java
+++ src/application/layout/mainController.java
@@ -1,18 +1,17 @@
 package application.layout;
 
 import application.ApplicationResource;
-import component.components.dialog.NewChapterDialog;
-import component.components.dialog.NewDocumentDialog;
-import component.components.dialog.NewEventCardDialog;
-import component.components.dialog.NewStoryLineDialog;
-import component.components.workspace.Workspace;
+import component.dialog.NewChapterDialog;
+import component.dialog.NewDocumentDialog;
+import component.dialog.NewEventCardDialog;
+import component.dialog.NewStoryLineDialog;
+import component.layouts.workspace.Workspace;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class mainController {
 
-    @FXML
-    private Workspace workspace;
+    @FXML private Workspace workspace;
 
     @FXML
     public void initialize() {
