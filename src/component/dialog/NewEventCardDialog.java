5d847abf8ff7e7c723f6f7001a78d2d456fe47bd
Nattamon Polvichai <joppy.pgh@gmail.com>
2020-04-11 18:12:21 +0700

    Shorten Dialog method name

--- src/component/dialog/NewEventCardDialog.java
+++ src/component/dialog/NewEventCardDialog.java
@@ -53,8 +53,8 @@ public class NewEventCardDialog extends Dialog {
     @FXML
     protected void initialize() {
         createButton.setDisable(true);
-        titleTextField.setOnKeyReleased((KeyEvent event) -> disableButtonWhenSomeTextFieldEmptyEmpty(createButton, titleTextField, descriptionTextField));
-        descriptionTextField.setOnKeyReleased((KeyEvent event) -> disableButtonWhenSomeTextFieldEmptyEmpty(createButton, titleTextField, descriptionTextField));
+        titleTextField.setOnKeyReleased((KeyEvent event) -> disableButtonWhenTextFieldEmpty(createButton, titleTextField, descriptionTextField));
+        descriptionTextField.setOnKeyReleased((KeyEvent event) -> disableButtonWhenTextFieldEmpty(createButton, titleTextField, descriptionTextField));
         createButton.setOnAction((ActionEvent e) -> {
             if (!isSomeEmpty(titleTextField, descriptionTextField)) {
                 AddeNewEventCard(titleTextField.getText(), descriptionTextField.getText());
