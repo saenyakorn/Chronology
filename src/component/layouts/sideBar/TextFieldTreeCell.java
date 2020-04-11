8dc3e65060a7590dfa9975d72a8d4c347f02cbb6
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-04-11 13:42:48 +0700

    Rename TextFieldTreeCell

---
+++ src/component/layouts/sideBar/TextFieldTreeCell.java
@@ -0,0 +1,74 @@
+package component.layouts.sideBar;
+
+import component.base.BasicStoryComponent;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeCell;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public final class TextFieldTreeCell extends TreeCell<BasicStoryComponent> {
+
+    TextField textField;
+
+    public TextFieldTreeCell() {
+        super();
+    }
+
+    @Override
+    public void startEdit() {
+        super.startEdit();
+        if (textField == null) {
+            createTextField();
+        }
+        setText(null);
+        setGraphic(textField);
+        textField.selectAll();
+    }
+
+    @Override
+    public void cancelEdit() {
+        super.cancelEdit();
+        setText(getItem().getName());
+        setGraphic(getTreeItem().getGraphic());
+    }
+
+    @Override
+    protected void updateItem(BasicStoryComponent item, boolean empty) {
+        super.updateItem(item, empty);
+
+        if (empty || item == null) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            System.out.println("updateItem: " + item.getName());
+            if (isEditing()) {
+                if (textField != null) {
+                    textField.setText(getString());
+                }
+                setText(null);
+                setGraphic(textField);
+            } else {
+                setText(getString());
+                setGraphic(getTreeItem().getGraphic());
+            }
+        }
+    }
+
+    private void createTextField() {
+        textField = new TextField(getString());
+        textField.setOnKeyReleased((KeyEvent event) -> {
+            System.out.println("On KeyPress");
+            if (event.getCode() == KeyCode.ENTER) {
+                BasicStoryComponent currentItem = getItem();
+                currentItem.setName(textField.getText());
+                commitEdit(currentItem);
+            } else if (event.getCode() == KeyCode.ESCAPE) {
+                cancelEdit();
+            }
+        });
+    }
+
+    private String getString() {
+        return getItem() == null ? "" : getItem().getName();
+    }
+}
