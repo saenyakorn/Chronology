5ac0e89e6030ffdbdd5d04ac4b4f128f3fe1c0b8
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-04-13 23:59:05 +0700

    Abled to create event card and added to chapters or storylines

--- src/component/dialog/NewEventCardDialog.java
+++ src/component/dialog/NewEventCardDialog.java
@@ -2,8 +2,15 @@ package component.dialog;
 
 import application.ApplicationResource;
 import application.SystemConstants;
+import component.base.BasicStoryComponent;
+import component.base.ComboBoxButtonCell;
+import component.base.ComboBoxListCell;
+import component.components.chapter.Chapter;
+import component.components.chapter.ChapterList;
 import component.components.document.Document;
 import component.components.eventCard.EventCard;
+import component.components.storyline.Storyline;
+import component.components.storyline.StorylineList;
 import component.layouts.workspace.Workspace;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,8 +18,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 
 import java.io.IOException;
 
@@ -26,13 +35,15 @@ public class NewEventCardDialog extends Dialog {
     Button createButton;
     @FXML
     Button cancelButton;
+    @FXML
+    HBox comboContainer;
 
     public NewEventCardDialog() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("NewEventCardDialog.fxml"));
         fxmlLoader.setController(this);
         try {
             Parent root = fxmlLoader.load();
-            stage.setTitle("Create New Storyline");
+            stage.setTitle("Create New Event");
             stage.setScene(new Scene(root, SystemConstants.DIALOG_PREF_HEIGHT, SystemConstants.DIALOG_PREF_WIDTH));
         } catch (IOException e) {
             e.printStackTrace();
@@ -61,5 +72,31 @@ public class NewEventCardDialog extends Dialog {
             }
         });
         cancelButton.setOnAction((ActionEvent e) -> stage.close());
+        CreateChapterComboBox();
+        CreateaStorylineComboBox();
+    }
+
+    private void CreateChapterComboBox() {
+        ComboBox<BasicStoryComponent> chapterCombo = new ComboBox<>();
+        Document document = ApplicationResource.getCurrentWorkspace().getCurrentDocument();
+        ChapterList chapters = document.getChapterList();
+        for (Chapter chapter : chapters) {
+            chapterCombo.getItems().add(chapter);
+        }
+        chapterCombo.setCellFactory(params -> new ComboBoxListCell());
+        chapterCombo.setButtonCell(new ComboBoxButtonCell());
+        comboContainer.getChildren().add(chapterCombo);
+    }
+
+    private void CreateaStorylineComboBox() {
+        ComboBox<BasicStoryComponent> storylineCombo = new ComboBox<>();
+        Document document = ApplicationResource.getCurrentWorkspace().getCurrentDocument();
+        StorylineList storylines = document.getStorylineList();
+        for (Storyline storyline : storylines) {
+            storylineCombo.getItems().add(storyline);
+        }
+        storylineCombo.setCellFactory(params -> new ComboBoxListCell());
+        storylineCombo.setButtonCell(new ComboBoxButtonCell());
+        comboContainer.getChildren().add(storylineCombo);
     }
 }
