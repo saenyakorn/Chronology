e5b8e57617170a6e0034f43720ac82c703b9b4a8
Saenyakorn <33742791+saenyakorn@users.noreply.github.com>
2020-04-14 20:29:51 +0700

    Added eventListener

--- src/component/layouts/viewer/Viewer.java
+++ src/component/layouts/viewer/Viewer.java
@@ -1,12 +1,24 @@
 package component.layouts.viewer;
 
+import application.ApplicationResource;
+import component.base.BasicStoryComponent;
 import component.components.document.Document;
+import component.components.storyline.Storyline;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.VBox;
 
 public class Viewer extends TabPane {
 
-    public Viewer() {
+    ScrollPane scrollPane;
+    VBox vBox;
 
+    public Viewer() {
+        scrollPane = new ScrollPane();
+        vBox = new VBox();
+        addEventListenderToNode();
     }
 
     public void addDocument(Document document) {
@@ -15,5 +27,23 @@ public class Viewer extends TabPane {
 
     public void removeDocument(Document document) {
         this.getTabs().remove(document);
+    }
+
+    private void addEventListenderToNode() {
+        this.setOnDragOver((DragEvent event) -> {
+            if (event.getDragboard().hasString()) {
+                event.acceptTransferModes(TransferMode.ANY);
+            }
+        });
+        this.setOnDragDropped((DragEvent event) -> {
+            String itemId = event.getDragboard().getString();
+            BasicStoryComponent item = ApplicationResource.getValueFromCurrentWorkspaceHashMap(itemId);
+            if (item instanceof Storyline) {
+                Storyline storyline = (Storyline) item;
+                vBox.getChildren().add(storyline);
+                scrollPane.setContent(vBox);
+                this.getSelectionModel().getSelectedItem().setContent(scrollPane);
+            }
+        });
     }
 }
