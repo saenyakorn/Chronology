6c10b019bb4af046afce8111732bf70c5b041f9a
Saenyakorn <33742791+saenyakorn@users.noreply.github.com>
2020-04-14 17:22:43 +0700

    Added hashMap between id and BasicStoryComponent for application and initialized it

--- src/application/ApplicationResource.java
+++ src/application/ApplicationResource.java
@@ -1,10 +1,18 @@
 package application;
 
+import component.base.BasicStoryComponent;
 import component.layouts.workspace.Workspace;
+
+import java.util.HashMap;
 
 public class ApplicationResource {
 
     private static Workspace currentWorkspace;
+    private static HashMap<String, BasicStoryComponent> hashMapBasicStoryComponents;
+
+    public static void initialize() {
+        hashMapBasicStoryComponents = new HashMap<>();
+    }
 
     public static Workspace getCurrentWorkspace() {
         return currentWorkspace;
@@ -12,5 +20,9 @@ public class ApplicationResource {
 
     public static void setCurrentWorkspace(Workspace currentWorkspace) {
         ApplicationResource.currentWorkspace = currentWorkspace;
+    }
+
+    public static HashMap<String, BasicStoryComponent> getHashMapBasicStoryComponents() {
+        return hashMapBasicStoryComponents;
     }
 }
