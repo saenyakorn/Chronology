27b75021ec680a292602d7ead2c965bb1f73bdce
Saenyakorn <33742791+saenyakorn@users.noreply.github.com>
2020-04-18 18:38:31 +0700

    Removed unnecessary method

--- src/component/components/document/DocumentList.java
+++ src/component/components/document/DocumentList.java
@@ -3,7 +3,7 @@ package component.components.document;
 import java.util.ArrayList;
 
 public class DocumentList {
-    private ArrayList<Document> documents;
+    private final ArrayList<Document> documents;
 
     public DocumentList() {
         this.documents = new ArrayList<>();
@@ -23,19 +23,6 @@ public class DocumentList {
 
     public void removeDocument(Document document) {
         documents.remove(document);
-    }
-
-    public void removeDocument(int index) {
-        documents.remove(index);
-    }
-
-    public void swap(Document document1, Document document2) {
-        int index1 = documents.indexOf(document1);
-        int index2 = documents.indexOf(document2);
-        if (index1 != -1 && index2 != -1) {
-            documents.set(index1, document2);
-            documents.set(index2, document1);
-        }
     }
 
     public int getSize() {
