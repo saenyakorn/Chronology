e03cbeaf44ce2ad76df20e70cd804dbff27e3ce4
Saenyakorn <33742791+saenyakorn@users.noreply.github.com>
2020-04-22 20:08:30 +0700

    Added loadFXML method

--- src/component/dialog/Dialog.java
+++ src/component/dialog/Dialog.java
@@ -1,10 +1,16 @@
 package component.dialog;
 
+import application.SystemConstants;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public abstract class Dialog {
     protected Stage stage;
@@ -14,6 +20,24 @@ public abstract class Dialog {
         stage.initModality(Modality.WINDOW_MODAL);
         stage.initStyle(StageStyle.UTILITY);
         stage.setAlwaysOnTop(true);
+    }
+
+    public void loadFXML(String title, String path) {
+        stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initStyle(StageStyle.UTILITY);
+        stage.setAlwaysOnTop(true);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(path));
+        fxmlLoader.setController(this);
+        try {
+            Parent root = fxmlLoader.load();
+            root.getStylesheets().add(getClass().getResource("Dialog.css").toExternalForm());
+            stage.setTitle(title);
+            stage.setScene(new Scene(root, SystemConstants.DIALOG_PREF_HEIGHT, SystemConstants.DIALOG_PREF_WIDTH));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void show() {
@@ -33,11 +57,7 @@ public abstract class Dialog {
     }
 
     public void disableButtonWhenTextFieldEmpty(Button button, TextField... args) {
-        if (isSomeEmpty(args)) {
-            button.setDisable(true);
-        } else {
-            button.setDisable(false);
-        }
+        button.setDisable(isSomeEmpty(args));
     }
 
 }
