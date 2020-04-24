a020fde2fa3c073620c1edb321fbb56ce800b8b8
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-04-24 23:56:55 +0700

    Created Default Color and remove others

--- src/application/SystemConstants.java
+++ src/application/SystemConstants.java
@@ -1,5 +1,7 @@
 package application;
 
+import javafx.scene.paint.Color;
+
 public class SystemConstants {
 
     /* Application constants */
@@ -8,30 +10,7 @@ public class SystemConstants {
     public static final int DIALOG_PREF_WIDTH = 300;
     public static final int DIALOG_PREF_HEIGHT = 400;
     public static final int EVENTCARD_PREF_WIDTH = 200;
-    
-    /* Styling constants */
-    public static final int SPACING = 8;
-    public static final int BORDER_RADIUS = 20;
-    public static final String DROP_SHADOW = " -fx-effect: dropshadow(gaussian, rgba(28,28,28,0.1), 20, 0, 0, 10);";
 
-    /* Font Constants */
-    public static final int HEADING_WEIGHT = 14;
-    public static final int REGULAR_WEIGHT = 12;
-
-    /*Color scheme constants*/
-    //Monochrome
-    public static final String WHITE = "#FFFFFF";
-    public static final String LIGHT_GRAY = "#F6F6F6";
-    public static final String MEDIUM_GRAY = "#4F4F4F";
-    public static final String DARK_GRAY = "#6B6B6B";
-    public static final String BLACK = "#1C1C1C";
-
-    //Default palette
-    public static final String PINK = "#FF76B8";
-    public static final String RED = "#FF3E60";
-    public static final String ORANGE = "#FFA63E";
-    public static final String LIME = "#94EE6A";
-    public static final String SKY_BLUE = "#3EC5FF";
-    public static final String LAVENDER = "#A587FB";
-
+    /* Default color */
+    public static final Color DEFAULT_COLOR = Color.rgb(99, 99, 99); // grey color
 }
