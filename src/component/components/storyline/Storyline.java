c891dc179ee1a43f55510383a97b11a4edc7069b
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-05-08 13:26:30 +0700

    Removed EventCardList

--- src/component/components/storyline/Storyline.java
+++ src/component/components/storyline/Storyline.java
@@ -1,7 +1,7 @@
 package component.components.storyline;
 
+import ability.Savable;
 import application.ApplicationResource;
-import application.SystemConstants;
 import colors.RandomColor;
 import component.base.BasicStoryComponent;
 import component.components.eventCard.EventCard;
@@ -13,17 +13,22 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
-import javafx.scene.input.*;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class Storyline extends BasicStoryComponent {
-    private final EventCardList eventCards;
     private ContextMenu contextMenu;
 
+    @FXML
+    private Pane root;
     @FXML
     private Line line;
     @FXML
@@ -31,10 +36,17 @@ public class Storyline extends BasicStoryComponent {
     @FXML
     private HBox storylineTitleContainer;
     @FXML
-    private EventCardList eventCardList;
+    private GridPane eventCardContainer;
 
     public Storyline() {
-        eventCards = new EventCardList();
+        loadFXML("Storyline.fxml");
+        initializeContextMenu();
+        initializeEventHandler();
+        setColor(RandomColor.getColor());
+    }
+
+    public Storyline(String componentID) {
+        super(componentID);
         loadFXML("Storyline.fxml");
         initializeContextMenu();
         initializeEventHandler();
@@ -43,7 +55,6 @@ public class Storyline extends BasicStoryComponent {
 
     public Storyline(String title, String description) {
         super(title, description);
-        eventCards = new EventCardList();
         loadFXML("Storyline.fxml");
         initializeContextMenu();
         initializeEventHandler();
@@ -52,7 +63,6 @@ public class Storyline extends BasicStoryComponent {
 
     public Storyline(String title, String description, Color color, TimePeriod timePeriod) {
         super(title, description, color, timePeriod);
-        eventCards = new EventCardList();
         loadFXML("Storyline.fxml");
         initializeContextMenu();
         initializeEventHandler();
@@ -61,7 +71,6 @@ public class Storyline extends BasicStoryComponent {
 
     public Storyline(String title, String description, Color color, TimePeriod timePeriod, EventCardList eventCards) {
         super(title, description, color, timePeriod);
-        this.eventCards = eventCards;
         loadFXML("Storyline.fxml");
         initializeContextMenu();
         initializeEventHandler();
@@ -83,6 +92,14 @@ public class Storyline extends BasicStoryComponent {
         });
     }
 
+    public Pane getDisplay() {
+        return root;
+    }
+
+    public GridPane getContainer() {
+        return eventCardContainer;
+    }
+
     @Override
     public void setTitle(String title) {
         super.setTitle(title);
@@ -94,33 +111,6 @@ public class Storyline extends BasicStoryComponent {
         super.setColor(color);
         line.setStroke(color);
         storylineTitle.setStyle("-fx-text-fill: " + colorToHex(color) + ";");
-        for (EventCard eventCard : eventCards) {
-            eventCard.setColor(color);
-        }
-    }
-
-    public EventCardList getEventCards() {
-        return eventCards;
-    }
-
-    public void addEventCard(EventCard eventCard) {
-        eventCards.addEventCard(eventCard);
-        eventCard.setStoryline(this);
-//        renderEventCards();
-        line.setEndX(line.getEndX() + (SystemConstants.EVENTCARD_PREF_WIDTH + 30));
-    }
-
-    public void renderEventCards() {
-//        eventCardList.getChildren().clear();
-//        for (EventCard eventCard : eventCards) {
-//            eventCardList.getChildren().add(eventCard);
-//        }
-    }
-
-    public void removeEventCard(EventCard eventCard) {
-        eventCards.removeEventCard(eventCard);
-        eventCardList.getChildren().remove(eventCard);
-        line.setEndX(line.getEndX() - (SystemConstants.EVENTCARD_PREF_WIDTH + 30));
     }
 
     @Override
@@ -130,61 +120,47 @@ public class Storyline extends BasicStoryComponent {
 
     @Override
     @SuppressWarnings("unchecked")
-    public JSONObject getJSONObject() {
-        JSONObject storyline = super.getJSONObject();
-        storyline.put("eventCardList", eventCards.getJSONArray());
-        return storyline;
+    public JSONObject writeJSONObject() {
+        JSONObject storylineObject = super.writeJSONObject();
+//        storylineObject.put("eventCardList", eventCards.writeJSONArray());
+        storylineObject.put("type", "Storyline");
+        return storylineObject;
     }
 
-    public static Storyline parseJSONObject(JSONObject storylineObject) {
-        String name = (String) storylineObject.get("name");
-        String description = (String) storylineObject.get("description");
-        Color color = Color.web((String) storylineObject.get("Color"));
-        TimePeriod timePeriod = TimePeriod.stringToTimePeriod((String) storylineObject.get("TimePeriod"));
-        EventCardList eventCards = EventCardList.parseJSONArray((JSONArray) storylineObject.get("eventCardList"));
-
-        return new Storyline(name, description, color, timePeriod, eventCards);
+    @Override
+    public Storyline readJSONObject(JSONObject storylineObject) {
+        Savable.printReadingMessage("storyline " + title);
+        super.readJSONObject(storylineObject);
+        JSONArray eventCardArray = (JSONArray) storylineObject.get("eventCardList");
+//        eventCards.readJSONArray(eventCardArray);
+        Savable.printReadingFinishedMessage("storyline " + title);
+        return this;
     }
 
     public void initializeEventHandler() {
-        setOnDragDetected((MouseEvent event) -> {
-            Dragboard dragboard = startDragAndDrop(TransferMode.MOVE);
-            dragboard.setDragView(snapshot(null, null));
-            ClipboardContent clipboardContent = new ClipboardContent();
-            clipboardContent.putString(componentId);
-            System.out.println(DataFormat.lookupMimeType("Storyline"));
-            dragboard.setContent(clipboardContent);
-            event.consume();
-        });
-        setOnDragOver((DragEvent event) -> {
+        root.setOnDragOver((DragEvent event) -> {
             if (event.getDragboard().hasString()) {
                 event.acceptTransferModes(TransferMode.MOVE);
             }
             event.consume();
         });
-        setOnDragDropped((DragEvent event) -> {
+        root.setOnDragDropped((DragEvent event) -> {
             String itemId = event.getDragboard().getString();
-            BasicStoryComponent item = ApplicationResource.getValueFromCurrentWorkspaceHashMap(itemId);
+            BasicStoryComponent item = ApplicationResource.getValueFromCurrentHashMap(itemId);
             if (item instanceof EventCard) {
                 EventCard eventCard = (EventCard) item;
-                ApplicationResource.getCurrentWorkspace().getActiveDocument().removeEventCard(eventCard);
-                addEventCard(eventCard);
-                ApplicationResource.update();
+                eventCard.setStoryline(this);
             }
             event.consume();
         });
-        setOnMousePressed((MouseEvent event) -> rightClickContextMenu(event));
+        root.setOnMousePressed((MouseEvent event) -> rightClickContextMenu(event));
     }
 
     private void rightClickContextMenu(MouseEvent event) {
-        if (contextMenu.isShowing()) {
-            contextMenu.hide();
-        }
+        contextMenu.hide();
         if (event.isSecondaryButtonDown()) {
             System.out.println("Storyline: " + event.getTarget());
-            contextMenu.show(this, event.getScreenX(), event.getScreenY());
-        } else {
-            contextMenu.hide();
+            contextMenu.show(root, event.getScreenX(), event.getScreenY());
         }
         event.consume();
     }
