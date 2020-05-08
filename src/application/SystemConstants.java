36ec830f01e1f384c875f187d7933c9672a54f56
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-05-08 15:48:00 +0700

    Moved DEFAULT_COLOR to GlobalColor

--- src/application/SystemConstants.java
+++ src/application/SystemConstants.java
@@ -1,7 +1,5 @@
 package application;
 
-import javafx.scene.paint.Color;
-
 public class SystemConstants {
 
     /* Application constants */
@@ -9,8 +7,5 @@ public class SystemConstants {
     public static final int SIDEBAR_PREF_WIDTH = 200;
     public static final int DIALOG_PREF_WIDTH = 300;
     public static final int DIALOG_PREF_HEIGHT = 400;
-    public static final int EVENTCARD_PREF_WIDTH = 200;
-
-    /* Default color */
-    public static final Color DEFAULT_COLOR = Color.rgb(99, 99, 99); // grey color
+    public static final int EVENT_CARD_PREF_WIDTH = 200;
 }
