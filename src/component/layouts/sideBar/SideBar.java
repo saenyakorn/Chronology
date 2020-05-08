fbf579a7b6ec8cad6649a296e25a69b8f2d8d109
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-05-08 13:25:37 +0700

    Split treeView to documentTreeView and basicStoryComponentTreeView. Changed reading data solution from document

--- src/component/layouts/sideBar/SideBar.java
+++ src/component/layouts/sideBar/SideBar.java
@@ -1,12 +1,15 @@
 package component.layouts.sideBar;
 
+import application.ApplicationResource;
 import application.SystemConstants;
 import component.base.BasicStoryComponent;
 import component.base.BasicStoryComponentTreeCell;
 import component.base.BlankBasicStoryComponent;
+import component.base.DocumentTreeCell;
 import component.components.chapter.Chapter;
 import component.components.chapter.ChapterList;
 import component.components.document.Document;
+import component.components.document.DocumentList;
 import component.components.eventCard.EventCard;
 import component.components.eventCard.EventCardList;
 import component.components.storyline.Storyline;
@@ -14,88 +17,141 @@ import component.components.storyline.StorylineList;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 public class SideBar extends ScrollPane {
 
-    private final TreeView<BasicStoryComponent> treeView;
+    // class flied
+    private final TreeView<Document> documentTreeView;
+    private final TreeView<BasicStoryComponent> basicStoryComponentTreeView;
+
+    // Icon for each item
+
+    private final Image eventCardIcon = new Image("file:res/icon/event_card_icon_24px.svg");
+    private final Image storylineIcon = new Image("file:res/icon/storyline_icon_24px.svg");
+    private final Image chapterIcon = new Image("file:res/icon/chapter_icon_24px.svg");
+    private final Image documentIcon = new Image("file:res/icon/document_icon_24px.svg");
 
     public SideBar() {
         // load css file
         getStylesheets().add(getClass().getResource("SideBar.css").toExternalForm());
 
-        // TreeView setting
-        treeView = new TreeView<>();
-        treeView.setEditable(true);
-        treeView.getStyleClass().add("tree-view");
-        treeView.setCellFactory(params -> new BasicStoryComponentTreeCell());
+        // basicStoryComponentTreeView setting
+        basicStoryComponentTreeView = new TreeView<>();
+        basicStoryComponentTreeView.setShowRoot(false);
+        basicStoryComponentTreeView.setEditable(true);
+        basicStoryComponentTreeView.getStyleClass().add("tree-view");
+        basicStoryComponentTreeView.setCellFactory(params -> new BasicStoryComponentTreeCell());
+
+        // documentTreeView setting
+        documentTreeView = new TreeView<>();
+        documentTreeView.setShowRoot(false);
+        documentTreeView.setEditable(true);
+        documentTreeView.getStyleClass().add("tree-view");
+        documentTreeView.setCellFactory(params -> new DocumentTreeCell());
 
         // VBox container setting
         VBox vBox = new VBox();
         vBox.setMaxHeight(Double.POSITIVE_INFINITY);
         vBox.getStyleClass().add("v-box");
-        vBox.getChildren().add(treeView);
+        vBox.getChildren().add(basicStoryComponentTreeView);
+        vBox.getChildren().add(documentTreeView);
 
         // scroll pane setting
-        this.getStyleClass().add("side-bar");
-        this.setFitToWidth(true);
-        this.setPrefWidth(SystemConstants.SIDEBAR_PREF_WIDTH);
-        this.setContent(vBox);
+        getStyleClass().add("side-bar");
+        setFitToWidth(true);
+        setPrefWidth(SystemConstants.SIDEBAR_PREF_WIDTH);
+        setContent(vBox);
+
+        // initialize TreeView
+        initializeBasicStoryComponentTreeView();
+        initializeTreeViewDocument();
     }
 
-    public void renderSideBar(Document document) {
-        EventCardList eventCardList = document.getEventCardList();
-        ChapterList chapterList = document.getChapterList();
-        StorylineList storylineList = document.getStorylineList();
-        BasicStoryComponent documentData = new BlankBasicStoryComponent(document.getText(), "none");
-        TreeItem<BasicStoryComponent> rootItem = new TreeItem<>(documentData);
-        createTreeItemFromNonChapterEventCard(rootItem, eventCardList);
-        createTreeItemFromChapterList(rootItem, chapterList);
-        createTreeItemFromStorylineList(rootItem, storylineList);
-        treeView.setRoot(rootItem);
+    private void initializeBasicStoryComponentTreeView() {
+        TreeItem<BasicStoryComponent> rootItem = new TreeItem<>();
+
+        // Event Card Tree Cell Construct -> index 0
+        TreeItem<BasicStoryComponent> eventCardTreeItemRoot = new TreeItem<>(new BlankBasicStoryComponent("Event Card", "None"), new ImageView(eventCardIcon));
+
+        // Storyline Tree Cell Construct -> index 1
+        TreeItem<BasicStoryComponent> storylineTreeItemRoot = new TreeItem<>(new BlankBasicStoryComponent("Storyline", "None"), new ImageView(storylineIcon));
+
+        // Chapter Tree Cell Construct -> index 2
+        TreeItem<BasicStoryComponent> chapterTreeItemRoot = new TreeItem<>(new BlankBasicStoryComponent("Chapter", "None"), new ImageView(chapterIcon));
+
+        rootItem.getChildren().addAll(eventCardTreeItemRoot, storylineTreeItemRoot, chapterTreeItemRoot);
+        basicStoryComponentTreeView.setRoot(rootItem);
     }
 
-    private void createTreeItemFromNonChapterEventCard(TreeItem<BasicStoryComponent> rootItem, EventCardList eventCardList) {
+    private void initializeTreeViewDocument() {
+        TreeItem<Document> rootItem = new TreeItem<>();
+        documentTreeView.setRoot(rootItem);
+    }
+
+    public void renderEventCardTreeItem(Document activeDocument) {
+        EventCardList eventCardList = activeDocument.getEventCardList();
+        TreeItem<BasicStoryComponent> eventCardTreeItemRoot = basicStoryComponentTreeView.getRoot().getChildren().get(0);
+        eventCardTreeItemRoot.getChildren().clear();
         for (EventCard eventCard : eventCardList) {
-            Chapter currentChapter = eventCard.getChapter();
-            if (currentChapter == null) {
-                TreeItem<BasicStoryComponent> treeItem = new TreeItem<>(eventCard);
-                rootItem.getChildren().add(treeItem);
+            if (eventCard.getStoryline() == null && eventCard.getChapter() == null) {
+                TreeItem<BasicStoryComponent> treeItem = new TreeItem<>(eventCard, new ImageView(eventCardIcon));
+                eventCardTreeItemRoot.getChildren().add(treeItem);
             }
         }
     }
 
-    private void createTreeItemFromChapterList(TreeItem<BasicStoryComponent> rootItem, ChapterList chapterList) {
-        for (Chapter chapter : chapterList) {
-            rootItem.getChildren().add(createTreeItemFromChapter(chapter));
-        }
-    }
-
-    private TreeItem<BasicStoryComponent> createTreeItemFromChapter(Chapter chapter) {
-        EventCardList eventCards = chapter.getEventCards();
-        TreeItem<BasicStoryComponent> rootItem = new TreeItem<>(chapter);
-        for (EventCard eventCard : eventCards) {
-            TreeItem<BasicStoryComponent> item = new TreeItem<>(eventCard);
-            rootItem.getChildren().add(item);
-        }
-        return rootItem;
-    }
-
-    private void createTreeItemFromStorylineList(TreeItem<BasicStoryComponent> rootItem, StorylineList storylineList) {
+    public void renderStorylineTreeItem(Document activeDocument) {
+        EventCardList eventCardList = activeDocument.getEventCardList();
+        StorylineList storylineList = activeDocument.getStorylineList();
+        TreeItem<BasicStoryComponent> storylineTreeItemRoot = basicStoryComponentTreeView.getRoot().getChildren().get(1);
+        storylineTreeItemRoot.getChildren().clear();
         for (Storyline storyline : storylineList) {
-            rootItem.getChildren().add(createTreeItemFromStoryline(storyline));
+            TreeItem<BasicStoryComponent> treeItem = new TreeItem<>(storyline, new ImageView(eventCardIcon));
+            for (EventCard eventCard : eventCardList) {
+                if (eventCard.getStoryline() == storyline) {
+                    TreeItem<BasicStoryComponent> childTreeItem = new TreeItem<>(eventCard, new ImageView(eventCardIcon));
+                    treeItem.getChildren().add(childTreeItem);
+                }
+            }
+            storylineTreeItemRoot.getChildren().add(treeItem);
         }
     }
 
-    private TreeItem<BasicStoryComponent> createTreeItemFromStoryline(Storyline storyline) {
-        EventCardList eventCards = storyline.getEventCards();
-        TreeItem<BasicStoryComponent> rootItem = new TreeItem<>(storyline);
-        for (EventCard eventCard : eventCards) {
-            TreeItem<BasicStoryComponent> item = new TreeItem<>(eventCard);
-            rootItem.getChildren().add(item);
+    public void renderChapterTreeItem(Document activeDocument) {
+        EventCardList eventCardList = activeDocument.getEventCardList();
+        ChapterList chapterList = activeDocument.getChapterList();
+        TreeItem<BasicStoryComponent> chapterTreeItemRoot = basicStoryComponentTreeView.getRoot().getChildren().get(2);
+        chapterTreeItemRoot.getChildren().clear();
+        for (Chapter chapter : chapterList) {
+            TreeItem<BasicStoryComponent> treeItem = new TreeItem<>(chapter, new ImageView(eventCardIcon));
+            for (EventCard eventCard : eventCardList) {
+                if (eventCard.getChapter() == chapter) {
+                    TreeItem<BasicStoryComponent> childTreeItem = new TreeItem<>(eventCard, new ImageView(eventCardIcon));
+                    treeItem.getChildren().add(childTreeItem);
+                }
+            }
+            chapterTreeItemRoot.getChildren().add(treeItem);
         }
-        return rootItem;
     }
 
+    public void renderDocumentTreeItem() {
+        TreeItem<Document> rootItem = documentTreeView.getRoot();
+        rootItem.getChildren().clear();
+        DocumentList documentList = ApplicationResource.getCurrentWorkspace().getDocumentList();
+        for (Document document : documentList) {
+            TreeItem<Document> treeItem = new TreeItem<>(document, new ImageView(documentIcon));
+            rootItem.getChildren().add(treeItem);
+        }
+    }
+
+    public void renderSideBar(Document document) {
+        renderEventCardTreeItem(document);
+        renderStorylineTreeItem(document);
+        renderChapterTreeItem(document);
+        renderDocumentTreeItem();
+    }
 
 }
