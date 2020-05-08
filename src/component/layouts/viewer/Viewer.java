8f32a807a3954c7a0833356226d5350c1910e51c
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-05-08 13:04:39 +0700

    Fixed unable to display storyline when changing tabs

--- src/component/layouts/viewer/Viewer.java
+++ src/component/layouts/viewer/Viewer.java
@@ -1,46 +1,25 @@
 package component.layouts.viewer;
 
 import application.ApplicationResource;
-import component.base.BasicStoryComponent;
 import component.components.document.Document;
 import component.components.eventCard.EventCard;
 import component.components.storyline.Storyline;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
 import javafx.scene.layout.VBox;
 
 public class Viewer extends TabPane {
 
-    private final VBox canvas;
     private final ScrollPane scrollPane;
     private final ContextMenu contextMenu;
 
     public Viewer() {
-        canvas = new VBox();
         scrollPane = new ScrollPane();
         contextMenu = new ContextMenu();
-
-        canvas.setStyle("-fx-background-color: rgba(165, 135, 251,0.2);");
-        canvas.setPrefSize(1500, 1500);
-        canvas.setMaxSize(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
-        canvas.setMinSize(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
-
-        System.out.println("LayoutX: " + canvas.getLayoutX());
-        System.out.println("LayoutY: " + canvas.getLayoutY());
-        System.out.println("Max H: " + canvas.getMaxHeight());
-        System.out.println("Max W: " + canvas.getMaxHeight());
-        System.out.println("Pref H: " + canvas.getPrefHeight());
-        System.out.println("Pref W: " + canvas.getPrefWidth());
-
         initializeContextMenu();
         initializeEventHandler();
-    }
-
-    public void renderViewer(Document document) {
-
     }
 
     public void addDocument(Document document) {
@@ -51,10 +30,12 @@ public class Viewer extends TabPane {
         this.getTabs().remove(document);
     }
 
-    public void addStorylineToCanvas(Storyline storyline) {
-        canvas.getChildren().add(storyline);
-        scrollPane.setContent(canvas);
-        getSelectionModel().getSelectedItem().setContent(scrollPane);
+    public void setContent(Node node) {
+        if (node instanceof VBox) {
+            VBox vBox = (VBox) node;
+            vBox.setOnMousePressed((MouseEvent event) -> rightClickContextMenu(event));
+            scrollPane.setContent(vBox);
+        }
     }
 
     private void createStoryline() {
@@ -72,30 +53,14 @@ public class Viewer extends TabPane {
     private void rightClickContextMenu(MouseEvent event) {
         contextMenu.hide();
         if (event.isSecondaryButtonDown()) {
+            System.out.println("Viewer: " + event.getTarget());
             contextMenu.show(this, event.getScreenX(), event.getScreenY());
         }
         event.consume();
     }
 
     private void initializeEventHandler() {
-        canvas.setOnDragOver((DragEvent event) -> {
-            if (event.getDragboard().hasString()) {
-                event.acceptTransferModes(TransferMode.MOVE);
-            }
-            event.consume();
-        });
-        canvas.setOnDragDropped((DragEvent event) -> {
-            String componentId = event.getDragboard().getString();
-            BasicStoryComponent obj = ApplicationResource.getValueFromCurrentWorkspaceHashMap(componentId);
-            if (obj instanceof Storyline) {
-                Storyline interestStoryline = (Storyline) obj;
-                interestStoryline.relocate(event.getX() - (interestStoryline.getPrefWidth() / 2), event.getY() - (interestStoryline.getPrefHeight() / 2));
-            }
-            event.consume();
-        });
-
         setOnMousePressed((MouseEvent event) -> rightClickContextMenu(event));
-        canvas.setOnMousePressed((MouseEvent event) -> rightClickContextMenu(event));
         scrollPane.setOnMousePressed((MouseEvent event) -> rightClickContextMenu(event));
     }
 
--- src/component/layouts/workspace/Workspace.java
+++ src/component/layouts/workspace/Workspace.java
@@ -1,6 +1,8 @@
 package component.layouts.workspace;
 
-import ablity.Savable;
+import ability.Savable;
+import ability.SavableAsJSONObject;
+import application.ApplicationResource;
 import component.base.BasicStoryComponent;
 import component.components.document.Document;
 import component.components.document.DocumentList;
@@ -10,22 +12,15 @@ import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-import java.util.HashMap;
-
-public class Workspace extends HBox implements Savable {
-    private final DocumentList documents;
-    private final Viewer viewer;
-    private final SideBar sideBar;
-    private final HashMap<String, BasicStoryComponent> hashMapBasicStoryComponents;
+public class Workspace extends HBox implements SavableAsJSONObject<Workspace> {
+    private final DocumentList documents = new DocumentList();
+    private final Viewer viewer = new Viewer();
+    private final SideBar sideBar = new SideBar();
 
     public Workspace() {
-        // Construct components
-        documents = new DocumentList();
-        viewer = new Viewer();
-        sideBar = new SideBar();
-        hashMapBasicStoryComponents = new HashMap<>();
-
         // Set Style
         HBox.setHgrow(viewer, Priority.ALWAYS);
 
@@ -33,7 +28,7 @@ public class Workspace extends HBox implements Savable {
         viewer.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends Tab> observableValue, Tab previousTab, Tab currentTab) -> {
             if (currentTab instanceof Document) {
                 Document selectedDocument = (Document) currentTab;
-                sideBar.renderSideBar(selectedDocument);
+                setActiveDocument(selectedDocument);
             }
         });
 
@@ -41,8 +36,12 @@ public class Workspace extends HBox implements Savable {
         this.getChildren().addAll(sideBar, viewer);
     }
 
-    public HashMap<String, BasicStoryComponent> getHashMapBasicStoryComponents() {
-        return hashMapBasicStoryComponents;
+    public Viewer getViewer() {
+        return viewer;
+    }
+
+    public SideBar getSideBar() {
+        return sideBar;
     }
 
     public DocumentList getDocumentList() {
@@ -54,8 +53,8 @@ public class Workspace extends HBox implements Savable {
     }
 
     public void setActiveDocument(Document document) {
+        viewer.getSelectionModel().select(document);
         sideBar.renderSideBar(document);
-        viewer.renderViewer(document);
     }
 
     public void addDocument(Document document) {
@@ -79,10 +78,6 @@ public class Workspace extends HBox implements Savable {
         viewer.removeDocument(document);
     }
 
-    public Viewer getViewer() {
-        return viewer;
-    }
-
     @Override
     public String toString() {
         return "Workspace: " + getId();
@@ -90,6 +85,40 @@ public class Workspace extends HBox implements Savable {
 
     @Override
     public String getJSONString() {
-        return documents.getJSONArray().toJSONString();
+        return this.writeJSONObject().toJSONString();
     }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public JSONObject writeJSONObject() {
+        JSONObject workspaceObject = new JSONObject();
+        workspaceObject.put("hashMap", ApplicationResource.getCurrentHashMapAsJSONObject());
+        workspaceObject.put("documentList", documents.writeJSONArray());
+        return workspaceObject;
+    }
+
+    @Override
+    public Workspace readJSONObject(JSONObject workspaceObject) {
+        Savable.printReadingMessage("workspace");
+        JSONObject hashMapObject = (JSONObject) workspaceObject.get("hashMap");
+        JSONArray documentArray = (JSONArray) workspaceObject.get("documentList");
+
+        Savable.printReadingMessage("hashMap");
+        hashMapObject.forEach((key, value) -> {
+            String componentID = (String) key;
+            JSONObject componentObject = (JSONObject) value;
+            BasicStoryComponent component = BasicStoryComponent.JSONObjectToBasicStoryComponent(componentID, componentObject);
+            System.out.println("Populating hashMap - key: " + key + ", type: " + component.getClass().getName());
+            ApplicationResource.putItemToCurrentHashMap(componentID, component.readJSONObject(componentObject));
+        });
+        Savable.printReadingFinishedMessage("hashMap");
+
+        Savable.printReadingMessage("documentList");
+        documents.readJSONArray(documentArray);
+        Savable.printReadingFinishedMessage("documentList");
+        Savable.printReadingFinishedMessage("workspace");
+
+        return this;
+    }
+
 }
