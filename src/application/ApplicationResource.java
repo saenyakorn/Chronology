d69ab163162a52520dc13974bdbe3df8d1ef1c8f
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-05-08 13:10:59 +0700

    Move hashMap to ApplicationResource

--- src/application/ApplicationResource.java
+++ src/application/ApplicationResource.java
@@ -1,15 +1,21 @@
 package application;
 
 import component.base.BasicStoryComponent;
+import component.components.chapter.Chapter;
 import component.components.document.Document;
+import component.components.eventCard.EventCard;
+import component.components.storyline.Storyline;
 import component.layouts.workspace.Workspace;
+import org.json.simple.JSONObject;
 
 import java.io.File;
+import java.util.HashMap;
 
 public class ApplicationResource {
 
-    private static File savedFile;
-    private static Workspace currentWorkspace;
+    private static final HashMap<String, BasicStoryComponent> hashMap = new HashMap<>(); //contains all basic story components
+    private static File savedFile = null;
+    private static Workspace currentWorkspace = null;
 
     public static void initialize() {
         savedFile = null;
@@ -38,12 +44,25 @@ public class ApplicationResource {
         ApplicationResource.currentWorkspace = currentWorkspace;
     }
 
-    public static void putItemToCurrentWorkspaceHashMap(String key, BasicStoryComponent value) {
-        ApplicationResource.currentWorkspace.getHashMapBasicStoryComponents().put(key, value);
+    public static void putItemToCurrentHashMap(String key, BasicStoryComponent value) {
+        ApplicationResource.hashMap.put(key, value);
     }
 
-    public static BasicStoryComponent getValueFromCurrentWorkspaceHashMap(String key) {
-        return ApplicationResource.currentWorkspace.getHashMapBasicStoryComponents().get(key);
+    public static BasicStoryComponent getValueFromCurrentHashMap(String key) {
+        return ApplicationResource.hashMap.get(key);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static JSONObject getCurrentHashMapAsJSONObject() {
+        JSONObject hashMapObject = new JSONObject();
+        ApplicationResource.hashMap.forEach((key, value) -> {
+            if (value instanceof EventCard || value instanceof Storyline || value instanceof Chapter) {
+                System.out.println("HashMap key: " + key + ", value = " + value.toString() + ", type = " + value.getClass().getName());
+                hashMapObject.put(key, value.writeJSONObject());
+            }
+        });
+        System.out.println();
+        return hashMapObject;
     }
 
     public static void newProject() {
@@ -52,5 +71,8 @@ public class ApplicationResource {
 
         // add template structure
         ApplicationResource.getCurrentWorkspace().addDocument(new Document("New Document"));
+
+        // render side bar
+        ApplicationResource.getCurrentWorkspace().getSideBar().renderSideBar(ApplicationResource.getCurrentWorkspace().getActiveDocument());
     }
 }
