c24933f90ff59e36dc879eefa3936b804c0fee36
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-05-08 13:11:44 +0700

    Removed eventCardList in Chapter

--- src/component/components/chapter/Chapter.java
+++ src/component/components/chapter/Chapter.java
@@ -1,7 +1,7 @@
 package component.components.chapter;
 
+import ability.Savable;
 import component.base.BasicStoryComponent;
-import component.components.eventCard.EventCard;
 import component.components.eventCard.EventCardList;
 import component.components.timeModifier.TimePeriod;
 import javafx.scene.paint.Color;
@@ -9,38 +9,25 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class Chapter extends BasicStoryComponent {
-    private final EventCardList eventCards;
 
     public Chapter() {
-        eventCards = new EventCardList();
+
+    }
+
+    public Chapter(String componentID) {
+        super(componentID);
     }
 
     public Chapter(String title, String description) {
         super(title, description);
-        eventCards = new EventCardList();
     }
 
     public Chapter(String title, String description, Color color, TimePeriod timePeriod) {
         super(title, description, color, timePeriod);
-        this.eventCards = new EventCardList();
     }
 
     public Chapter(String title, String description, Color color, TimePeriod timePeriod, EventCardList eventCards) {
         super(title, description, color, timePeriod);
-        this.eventCards = eventCards;
-    }
-
-    public EventCardList getEventCards() {
-        return eventCards;
-    }
-
-    public void addEventCard(EventCard eventCard) {
-        eventCards.addEventCard(eventCard);
-        eventCard.setChapter(this);
-    }
-
-    public void removeEventCard(EventCard eventCard) {
-        eventCards.removeEventCard(eventCard);
     }
 
     @Override
@@ -48,20 +35,22 @@ public class Chapter extends BasicStoryComponent {
         return title;
     }
 
-    @Override @SuppressWarnings("unchecked")
-    public JSONObject getJSONObject() {
-        JSONObject chapter = super.getJSONObject();
-        chapter.put("eventCardList", eventCards.getJSONArray());
-        return chapter;
+    @Override
+    @SuppressWarnings("unchecked")
+    public JSONObject writeJSONObject() {
+        JSONObject chapterObject = super.writeJSONObject();
+//        chapterObject.put("eventCardList", eventCards.writeJSONArray());
+        chapterObject.put("type", "Chapter");
+        return chapterObject;
     }
 
-    public static Chapter parseJSONObject(JSONObject chapterObject) {
-        String name = (String) chapterObject.get("name");
-        String description = (String) chapterObject.get("description");
-        Color color = Color.web((String) chapterObject.get("Color"));
-        TimePeriod timePeriod = TimePeriod.stringToTimePeriod((String) chapterObject.get("TimePeriod"));
-        EventCardList eventCards = EventCardList.parseJSONArray((JSONArray) chapterObject.get("eventCardList"));
-
-        return new Chapter(name, description, color, timePeriod, eventCards);
+    @Override
+    public Chapter readJSONObject(JSONObject chapterObject) {
+        Savable.printReadingMessage("chapter " + title);
+        super.readJSONObject(chapterObject);
+        JSONArray eventCardArray = (JSONArray) chapterObject.get("eventCardList");
+//        eventCards.readJSONArray(eventCardArray);
+        Savable.printReadingFinishedMessage("chapter " + title);
+        return this;
     }
 }
