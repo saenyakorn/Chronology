017f8b99ea3daaa87e754e7d914f08e87608677b
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-05-11 16:18:19 +0700

    Added os condition for displaying application

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,8 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    private final String os = System.getProperty("os.name");
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -16,11 +18,16 @@ public class Main extends Application {
     @Override
     public void start(Stage mainWindow) throws Exception {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("layout/mainWindow.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("layout/MainWindow.fxml"));
             root.getStylesheets().add(getClass().getResource("StylingConstant.css").toExternalForm());
             mainWindow.setTitle(SystemConstants.APP_NAME);
             mainWindow.setScene(new Scene(root, mainWindow.getMaxWidth(), mainWindow.getMaxHeight()));
-            mainWindow.initStyle(StageStyle.TRANSPARENT);
+            if (os != null && os.startsWith("Windows")) {
+                mainWindow.initStyle(StageStyle.TRANSPARENT);
+            } else {
+                mainWindow.initStyle(StageStyle.UNDECORATED);
+            }
+            mainWindow.resizableProperty().setValue(true);
             mainWindow.setMaximized(true);
             mainWindow.show();
         } catch (Exception e) {
