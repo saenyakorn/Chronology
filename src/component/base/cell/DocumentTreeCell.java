6eb0f8e1fd4b8c759838a3c988c3b506d41164c1
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-05-15 23:29:42 +0700

    Modify code

--- src/component/base/cell/DocumentTreeCell.java
+++ src/component/base/cell/DocumentTreeCell.java
@@ -2,9 +2,6 @@ package component.base.cell;
 
 import colors.GlobalColor;
 import component.components.document.Document;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.SVGPath;
 import utils.ApplicationUtils;
@@ -12,7 +9,6 @@ import utils.SystemUtils;
 
 public final class DocumentTreeCell extends CustomTreeCell<Document> {
 
-    private TextField textField;
     private final SVGPath documentIcon = SystemUtils.getIconSVG("document_icon_24px.svg");
 
     public DocumentTreeCell() {
@@ -26,19 +22,11 @@ public final class DocumentTreeCell extends CustomTreeCell<Document> {
     @Override
     public void startEdit() {
         super.startEdit();
-        if (textField == null) {
-            createTextField();
-        }
-        setText(null);
-        setGraphic(textField);
-        textField.selectAll();
     }
 
     @Override
     public void cancelEdit() {
         super.cancelEdit();
-        setText(getItem().getName());
-        setGraphic(getTreeItem().getGraphic());
     }
 
     @Override
@@ -48,44 +36,20 @@ public final class DocumentTreeCell extends CustomTreeCell<Document> {
             setText(null);
             setGraphic(null);
         } else {
-            if (isEditing()) {
-                if (textField != null) {
-                    textField.setText(getString());
-                }
-                setText(null);
-                setGraphic(textField);
-            } else {
-                setText(getString());
-                documentIcon.setFill(GlobalColor.DEFAULT_COLOR);
-                setGraphic(documentIcon);
-            }
+            setText(getItem().getName());
+            documentIcon.setFill(GlobalColor.DEFAULT_COLOR);
+            setGraphic(documentIcon);
         }
-    }
-
-    private void createTextField() {
-        textField = new TextField(getString());
-        textField.setOnKeyReleased((KeyEvent event) -> {
-            if (event.getCode() == KeyCode.ENTER) {
-                Document currentItem = getItem();
-                currentItem.setName(textField.getText());
-                commitEdit(currentItem);
-            } else if (event.getCode() == KeyCode.ESCAPE) {
-                cancelEdit();
-            }
-        });
     }
 
     @Override
     protected void initializeEventHandler() {
         super.initializeEventHandler();
         setOnMouseClicked((MouseEvent event) -> {
-            if (getItem() instanceof Document) {
+            if (getItem() != ApplicationUtils.getCurrentWorkspace().getActiveDocument()) {
                 ApplicationUtils.getCurrentWorkspace().setActiveDocument(getItem());
             }
         });
     }
 
-    private String getString() {
-        return getItem() == null ? "" : getItem().getName();
-    }
 }
