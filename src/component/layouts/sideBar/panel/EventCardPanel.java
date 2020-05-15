d55b0e62a1cd7111fd8d6fd360f9e9864070fa6c
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-05-15 20:49:01 +0700

    Added component adding button

--- src/component/layouts/sideBar/panel/EventCardPanel.java
+++ src/component/layouts/sideBar/panel/EventCardPanel.java
@@ -2,13 +2,19 @@ package component.layouts.sideBar.panel;
 
 import component.base.cell.EventCardTreeCell;
 import component.components.eventCard.EventCard;
+import component.dialog.initialize.NewEventCardDialog;
 import utils.SystemUtils;
 
 public class EventCardPanel extends Panel<EventCard> {
 
     public EventCardPanel() {
-        setHeader(SystemUtils.EVENTCARD_DEMO_HEADER);
+        setHeader(SystemUtils.EVENT_CARD_DEMO_HEADER);
         getTreeView().setCellFactory(param -> new EventCardTreeCell());
+    }
+
+    @Override
+    public void onButtonClick() {
+        new NewEventCardDialog().show();
     }
 
 }
